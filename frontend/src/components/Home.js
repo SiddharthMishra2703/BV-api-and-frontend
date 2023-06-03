a03a2ced220f5fb7fd2bdf249f57a2de156a60a8
Siddharth Mishra <siddharthmishra2703@gmail.com>
2023-06-03 19:55:13 +0530

    Serever Status Added

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,11 +1,39 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import HomeFirst from './HomeFirst'
 import HomeSecond from './HomeSecond'
 
 
 export default function Home() {
+    const [userData, setUserData] = useState({message:"Server Is Loading..."});
+
+    const getData = async () => {
+        try {
+            const res = await fetch(process.env.REACT_APP_API + '/', {
+                method: "GET",
+                headers: {
+                    Accept: "appllication/json",
+                    "Content-Type": "application/json"
+                }
+            });
+
+            const data = await res.json();
+            setUserData(data);
+
+            if (!res.status === 200) {
+                const error = new Error(res.error);
+                throw error;
+            }
+        } catch (err) {
+            console.log(err);
+        }
+    }
+    useEffect(() => {
+        getData();
+    }, []);
+    console.log(userData);
   return (
     <>
+    <p>{userData.message}</p>
       <HomeFirst/>
       <HomeSecond/>
     </>
