1de0bc254e21a09691a6f710d1bb5e1da2bee780
Siddharth Mishra <107067738+SiddharthMishra2703@users.noreply.github.com>
2023-06-03 18:48:59 +0530

    changes

---
+++ frontend/src/components/Logout.js
@@ -0,0 +1,38 @@
+import React, { useEffect , useContext } from 'react'
+import { useNavigate } from 'react-router-dom';
+import { UserContext } from '../App';
+
+export default function Logout() {
+
+  const { dispatch} = useContext(UserContext);
+
+    const navigate = useNavigate();
+    document.cookie = "not";
+    console.log(document.cookie);
+
+    useEffect(() => {
+      fetch(process.env.REACT_APP_API + '/logout', {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json"
+        },
+      }).then((res)=>{
+        dispatch({type:"USER" , payload:false});
+
+        navigate('/login', {replace: true});
+        if (res.status !== 200) {
+          const error = new Error(res.error);
+          throw error;
+        }
+      }).catch((err)=>{
+        console.log(err);
+      })
+    });
+  
+  return (
+    <div className='text-center text-primary'>
+      <h1>Thank You For visting.</h1>
+      <h3>Come back soon.</h3>
+    </div>
+  )
+}
